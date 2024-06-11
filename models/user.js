b3701971d8fb17e8e84a38bebaf45bf8acaea2aa
NIGUELCLARK <141190541+NIGUELCLARK@users.noreply.github.com>
2024-06-12 00:43:52 +0100

    Add files via upload

--- models/user.js
+++ models/user.js
@@ -1,40 +1,24 @@
-// models/User.js
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: [true, 'Please tell us your name!']
-    },
-    email: {
-        type: String,
-        required: [true, 'Please provide your email'],
-        unique: true,
-        lowercase: true
-    },
-    password: {
-        type: String,
-        required: [true, 'Please provide a password'],
-        minlength: 8,
-        select: false
-    },
-    role: {
-        type: String,
-        enum: ['recruiter', 'talent'],
-        default: 'talent'
-    }
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  role: { type: String, enum: ['recruiter', 'employee'], required: true },
+  profilePicture: { type: String }
 });
 
+
+
+
+// Hash password before saving
 userSchema.pre('save', async function (next) {
-    if (!this.isModified('password')) return next();
-    this.password = await bcrypt.hash(this.password, 12);
-    next();
+  if (!this.isModified('password')) return next();
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
 });
-
-userSchema.methods.correctPassword = async function (candidatePassword, userPassword) {
-    return await bcrypt.compare(candidatePassword, userPassword);
-};
 
 const User = mongoose.model('User', userSchema);
 
